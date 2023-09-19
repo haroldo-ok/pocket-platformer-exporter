269be7f71c59935cbb736d28de17669486f4f9e2
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-09-19 19:21:47 -0300

    Extract JS snippet for level data.

---
+++ index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+// Adapted from: https://lage.us/Javascript-Get-String-Between-Strings.html
+const stringBetweenStrings = (str, startStr, endStr) => {
+    pos = str.indexOf(startStr) + startStr.length;
+    return str.substring(pos, str.indexOf(endStr, pos));
+}
+
+const exampleHtml = fs.readFileSync('example-project.html', 'utf8');
+
+console.log(stringBetweenStrings(exampleHtml, '//initialLevelDataStart', '//initialLevelDataEnd'));

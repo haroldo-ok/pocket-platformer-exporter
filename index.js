a9fb5e9a04cf1b8bf3343185bc97f46d530c8230
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-09-20 18:18:05 -0300

    Install and test the `jimp` package.

--- index.js
+++ index.js
@@ -31,3 +31,29 @@ const player = parseSection(exampleHtml, 'changedPlayerAttributes', /player\["(.
 fs.writeFileSync('generated.json', stringify({ world, sprites, player }, { maxLength: 160 }));
 	
 console.log('See "generated.json"');
+
+
+const Jimp = require('jimp');
+
+
+let imageData = [
+  [ 0xFF0000FF, 0xFF0000FF, 0xFF000000 ],
+  [ 0xFF0000FF, 0x00FF00FF, 0xFF0000FF ],
+  [ 0xFF0000FF, 0xFF0000FF, 0x0000FFFF ]
+];
+
+
+// See https://stackoverflow.com/a/42635011/679240
+let image = new Jimp(3, 3, function (err, image) {
+  if (err) throw err;
+
+  imageData.forEach((row, y) => {
+    row.forEach((color, x) => {
+      image.setPixelColor(color, x, y);
+    });
+  });
+
+  image.write('test.png', (err) => {
+    if (err) throw err;
+  });
+});

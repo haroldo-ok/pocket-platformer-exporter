f3231052d5845ec29dcbe41b4b2068f030e25e83
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-09-21 18:08:05 -0300

    Copy example map and tileset from `dragon-blaster`

---
+++ tileset.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="tileset" tilewidth="16" tileheight="16" tilecount="48" columns="16">
+ <image source="../img/tileset.png" width="256" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="boolvalue" type="bool" value="true"/>
+   <property name="floatValue" type="float" value="1234.56"/>
+   <property name="test1" value="example"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="anumber" type="float" value="2222.3"/>
+  </properties>
+ </tile>
+</tileset>

ef80834316a9da6decc2d0c5826727d0d7f4ac51
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-02 19:17:20 -0300

    Generate image into a buffer.

--- src/tileset.js
+++ src/tileset.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const TILESET_WIDTH_TILES = 32;
+
 const prepareTileSetData = ({ sprites }) => Object.entries(sprites)
 	.map(([key, { animation, ...rest }]) => ({ 
 		metaData: { key, ...rest }, 
@@ -15,5 +17,45 @@ const prepareTileSetData = ({ sprites }) => Object.entries(sprites)
 			targetIndex: targetTileCount
 		}]
 	}), { sourceTileCount: 0, targetTileCount: 0, tiles: [] });
+	
+	
+const generateTileSetImage = async ({ sprites }) => {
+	const tileSetData = prepareTileSetData({ sprites });
+	
+	// Convert tileset to image
 
-module.exports = { prepareTileSetData };
+	const Jimp = require('jimp');
+
+	return new Promise((resolve, reject) => {
+		// See https://stackoverflow.com/a/42635011/679240	
+		const imageWidth = TILESET_WIDTH_TILES * 8;
+		const imageHeight = Math.ceil(tileSetData.targetTileCount / TILESET_WIDTH_TILES) * 8;
+		let image = new Jimp(imageWidth, imageHeight, function (err, image) {
+			if (err) throw err;
+
+			tileSetData.tiles.forEach(({ targetIndex, frames }) => {
+				frames.forEach((frame, frameNumber) => {
+					const tileNumber = targetIndex + frameNumber;
+					const offset = {
+						x: (tileNumber % TILESET_WIDTH_TILES) * 8,
+						y: Math.floor(tileNumber / TILESET_WIDTH_TILES) * 8
+					};
+					
+					frame.forEach((row, y) => {
+						row.forEach((color, x) => {
+						  image.setPixelColor(color, x + offset.x, y + offset.y);
+						});
+					});
+				});				
+			});
+
+			image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+				if (err) throw err;
+				resolve(buffer);
+			});
+		});
+	});
+
+};
+
+module.exports = { prepareTileSetData, generateTileSetImage };
--- src/tileset.unit.test.js
+++ src/tileset.unit.test.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const { prepareTileSetData } = require('./tileset');
+const { prepareTileSetData, generateTileSetImage } = require('./tileset');
 
 const JSON_SOURCE = require('./mocks/example-project.json');
 
@@ -11,4 +11,11 @@ test('convert the sprite data into a convenient structure for using in other ste
 	const tileSetData = prepareTileSetData(JSON_SOURCE);
 	
 	expect(tileSetData).toBeTruthy();
+});
+
+test('generate a PNG image from the sprite data', async () => {
+	const imageBuffer = await generateTileSetImage(JSON_SOURCE);
+	
+	expect(Buffer.isBuffer(imageBuffer)).toBeTruthy();
+	expect(imageBuffer).toEqual(await fs.promises.readFile(path.join(__dirname, 'mocks/example-project.png')));
 });

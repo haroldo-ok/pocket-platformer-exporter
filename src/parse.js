e68aca4ace1434baf269bd9acd83345341732a64
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-01 21:26:35 -0300

    Create unit test for parsing.

---
+++ src/parse.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const parsePocketPlatformer = () => {};
+
+module.exports = { parsePocketPlatformer };
---
+++ src/parse.unit.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const { parsePocketPlatformer } = require('./parse');
+
+test('parse the PP file', () => {
+	const source = fs.readFileSync(path.join(__dirname, 'mocks/example-project.html'), 'utf8');
+	
+	parsePocketPlatformer(source);
+	
+	/*
+	const { world, sprites, player } = parsePocketPlatformer(source);
+	
+	expect(world).toBeTruthy();
+	expect(sprites).toBeTruthy();
+	expect(player).toBeTruthy();
+	*/
+});

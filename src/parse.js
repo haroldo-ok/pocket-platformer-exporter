c897dd799fda7ee49bc78190e60c6044677cb5fc
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-01 21:31:39 -0300

    Copy parsing code to `parse.js`.

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const parsePocketPlatformer = () => {};
+// Adapted from: https://lage.us/Javascript-Get-String-Between-Strings.html
+const stringBetweenStrings = (str, startStr, endStr) => {
+    const pos = str.indexOf(startStr) + startStr.length;
+    return str.substring(pos, str.indexOf(endStr, pos));
+}
+
+const extractJsSnippet = (html, delimiter) => stringBetweenStrings(html, `//${delimiter}Start`, `//${delimiter}End`);
+
+const parseJsValue = original => JSON.parse(original.trim()
+	// Remove trailing ';'
+	.replace(/;$/, '')
+	// Parse 'unescape("foo")' into a JSON string
+	.replace(/unescape\((.*)\)/, (_, str) => JSON.stringify(unescape(JSON.parse(str)))));
+
+const parseSnippet = (snippet, regex) => Object.fromEntries([...snippet.matchAll(regex)]
+	.map(match => [match[1], parseJsValue(match[2])]));
+	
+const parseSection = (html, delimiter, regex) => parseSnippet(extractJsSnippet(html, delimiter), regex);
+
+const parsePocketPlatformer = (sourceHtml) => {
+	const world = parseSection(sourceHtml, 'initialLevelData', /WorldDataHandler\.(\w+)\s*=\s*(.*)\s*[;\n]/gm);
+	const sprites = parseSection(sourceHtml, 'changedSprites', /SpritePixelArrays\["(.*?)"\]\s*=\s*(.*?);/g);
+	const player = parseSection(sourceHtml, 'changedPlayerAttributes', /player\["(.*?)"\]\s*=\s*(.*?);/g);
+	
+	return { world, sprites, player };
+};
 
 module.exports = { parsePocketPlatformer };

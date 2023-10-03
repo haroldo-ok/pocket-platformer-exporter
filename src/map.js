ab213f10b5dbfd287eddfa16f57b8f6c2fb30544
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-03 19:12:20 -0300

    Prepare level data for a single level.

--- src/map.js
+++ src/map.js
@@ -1,5 +1,28 @@
 'use strict';
 
-const prepareLevelData = () => 'TODO';
+const { prepareTileSetData } = require('./tileset');
+
+const prepareLevelData = ({ sprites }, { tileData, levelObjects }, levelIndex) => {
+	const tileSetData = prepareTileSetData({ sprites });
+	const tilesPerName = Object.fromEntries(tileSetData.tiles.map(tile => [tile.metaData.name, tile]));
+	return { 
+		levelNumber: levelIndex + 1,
+		width: tileData[0].length,
+		height: tileData.length,
+		map: tileData.map(row => row.map(cell => {
+			if (!cell) return 0;
+			
+			const cellName = cell === 1 ? 'edge' : cell;
+			const tile = tilesPerName[cellName];
+			return tile ? tile.targetIndex + 1 : 0;
+		})),
+		objects: (levelObjects || []).map(({ type, ...rest }, objectIndex) => ({
+			id: objectIndex + 2,
+			type,
+			gid: (tilesPerName[type] || {}).targetIndex + 1,
+			...rest,
+		}))
+	};
+};
 
 module.exports = { prepareLevelData };
--- src/map.unit.test.js
+++ src/map.unit.test.js
@@ -7,8 +7,9 @@ const { prepareLevelData } = require('./map');
 
 const JSON_SOURCE = require('./mocks/example-project.json');
 
-test('convert the level data into a convenient structure for using in other steps', () => {
-	const levelData = prepareLevelData(JSON_SOURCE, JSON_SOURCE.world.levels[2]);
+test('convert the data of a single level into a convenient structure for using in other steps', () => {
+	const levelData = prepareLevelData(JSON_SOURCE, JSON_SOURCE.world.levels[2], 2);
 	
 	expect(levelData).toBeTruthy();
+	expect(levelData.levelNumber).toBe(3);
 });

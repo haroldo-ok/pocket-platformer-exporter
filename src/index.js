c135f3024f76d2f57f23bf74f5f287fa0aa9ad43
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-04 20:07:05 -0300

    Remove dead code.

--- src/index.js
+++ src/index.js
@@ -7,32 +7,6 @@ const { parsePocketPlatformer } = require('./parse');
 const { generateTileSetImage, generateTiledTileSet } = require('./tileset');
 const { generateTiledMaps } = require('./map');
 
-const removeWhenDone = () => {	
-	const exampleHtml = fs.readFileSync('src/mocks/example-project.html', 'utf8');
-
-	const { world, sprites, player } = parsePocketPlatformer(exampleHtml);
-
-	fs.writeFileSync('generated.json', stringify({ world, sprites, player }, { maxLength: 160 }));
-		
-	console.log('See "generated.json"');
-
-
-	// Convert tileset to image
-
-	generateTileSetImage({ sprites }).then(buffer => fs.writeFileSync('example-project.png', buffer));
-
-
-	// Convert tileset to TSX
-
-	generateTiledTileSet({ world, sprites, player }, { filePrefix: 'example-project' })
-		.then(xml => fs.writeFileSync('example-project.tsx', xml));
-
-
-	// Convert maps to TMX
-
-	generateTiledMaps({ world, sprites }, { filePrefix: 'example-project' })
-		.forEach(({name, data}) => fs.writeFileSync(name, data));
-}
 
 const parseToObject = async (htmlContent) => parsePocketPlatformer(htmlContent);
 const convertToJson = async (htmlContent) => stringify(await parseToObject(htmlContent), { maxLength: 160 }); 
@@ -74,4 +48,4 @@ const saveToTiled = async (htmlPath, targetDir, options = {}) => {
 	}
 };
 
-module.exports = { parseToObject, convertToJson, saveToJson, saveToTiled };
+module.exports = { parseToObject, convertToJson, saveToJson, convertToTiled, saveToTiled };
